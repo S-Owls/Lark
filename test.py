68806ead534fc90cb39fc3e2a776146ed024406f
seungtaek94 <seungtaek.kim.94@gmail.com>
2022-11-08 16:03:13 +0900

    test

--- test.py
+++ test.py
@@ -6,3 +6,9 @@ if __name__=="__main__":
     parser.add_argument("--test", type=str, help="test")
     args = parser.parse_args()
     print(args.test)
+    
+    
+    if args.test == "testtest":
+        print("saaaameeee")
+    else:
+        print("diiiffffff")
