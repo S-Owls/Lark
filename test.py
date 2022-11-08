e10a95eaad8d9c670fc502d3734233e932257c0a
seungtaek94 <seungtaek.kim.94@gmail.com>
2022-11-08 16:01:29 +0900

    test

---
+++ test.py
@@ -0,0 +1,8 @@
+import argparse
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", type=str, help="test")
+    args = parser.parse_args()
+    print(args.test)
