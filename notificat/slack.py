a2ed72c639d8dfcfbc97c1471487752c0f036cc2
seungtaek94 <seungtaek.kim.94@gmail.com>
2022-11-12 16:04:38 +0900

    Implement slackAPI

--- notificat/slack.py
+++ notificat/slack.py
@@ -1,6 +1,6 @@
 import sys
 import requests
-from typing import Dict
+from typing import Dict, Any
 
 from notificat.base import Notificat
 
@@ -12,71 +12,69 @@ class SlackAPI(Notificat):
         self.token = token
         
         
-    def send(self, channelID:str, message:str):
+    def send(self, channelID:str, message:str) -> Dict[str, Any]:
         """Send message to channel.
 
         Args:
             channelID (str): Channel ID where to send message.
             message (str): The message that send to be the channel.
+            
+        Returns:
+            Dict[str, Any]: Response to a request. If the request is successful, the value with key "ok" is "True".
         """
         response = requests.post("https://slack.com/api/chat.postMessage",
                                  headers={"Content-Type": "application/json",
                                           "Authorization": f"Bearer {self.token}"},
-                                 json={"text": message, "channel": channelID})
+                                 json={"text": message, "channel": channelID})        
+        return response.json()
         
-        self.check_error(response)
-        
-    # TO DO:
-    def get_msg(self, channelID:str, max:int):
-        pass
     
-    # TO DO:
-    def get_latest_msg(self, channelID:str, max:int):
-        pass
+    def get_msg(self, channelID:str, max:int) -> Dict[str, Any]:
+        """Get messages from the input channel.          
+            If channel has not enough messages to get, The number of message what you get can be less then `max` value.
+
+        Args:
+            channelID (str): Channel ID where to get message.
+            max (int): Maximum number of messages what you get.
+
+        Returns:
+            Dict[str, Any]: Response to a request. If the request is successful, the value with key "ok" is "True".
+        """
+        response = requests.get(f'https://slack.com/api/conversations.history?channel={channelID}&limit={max}',
+                                headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {self.token}'})
+        
+        return response.json()
+    
+    
+    def get_latest_msg(self, channelID:str) -> Dict[str, Any]:
+        """Get last last message from input channel.
+        
+        Args:
+            channelID (str):  Channel ID where to get message.
+
+        Returns:
+            Dict[str, Any]: Response to a request. If the request is successful, the value with key "ok" is "True".
+        """
+        response = requests.get(f'https://slack.com/api/conversations.history?channel={channelID}&limit=1',
+                                 headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {self.token}'})        
+        return response.json()
             
             
-    def get_channel_list(self, max:int=1) -> Dict[str, str]:
+    def get_channel_list(self, max:int=1) -> Dict[str, Any]:
         """Get channel list.
 
         Args:
             max (int): Maximum number of channels to get. Defaults to 1.
 
         Returns:
-            Dict[str, str]: The return value that channel list. 
-                        Output dictionary shape like: {"id":"name"}.
+            Dict[str, Any]: Response to a request. If the request is successful, the value with key "ok" is "True".
         """
         response = requests.get(f"https://slack.com/api/conversations.list?limit={max}",
                                 headers={"Content-Type": "application/json",
                                          "Authorization": f"Bearer {self.token}"})        
         
-        self.check_error(response)
-        
-        return {c["id"]:c["name"] for c in response.json()["channels"]}
-        
-        
-    def check_error(self, response:requests.Response):
-        """Check any error from ``requests.Response`` object.
-
-        Args:
-            response (requests.Response): Reesult object of ``requests.get()`` and ``requests.post()``.
-
-        Raises:
-            Exception: When ``GET/POST`` message get an ``status_code`` other than 200.
-                    Or get ``false`` response with error message from slack api.
-        """
-        method_name = sys._getframe(1).f_code.co_name
-        
-        if not response.status_code == 200:
-            print(f"[Error] func:Slack.{method_name} - Faild to get response from API(ok code != 200)")
-            raise Exception("[Error] Faild to get response from API(ok code != 200)")
-        
-        response = response.json()
-        if not response["ok"]:
-            print(f"[Error] func:Slack.{method_name} - {response}")
-            
-            # TO DO:
-            # Determining the error message
-            raise Exception("[Error] Failed to GET/POST ~~.")
+        return response.json()    
+    
             
         
         
--- tests/test_SlackAPI.py
+++ tests/test_SlackAPI.py
@@ -2,8 +2,8 @@ import os
 import datetime
 from unittest import TestCase
 
-
 from notificat import SlackAPI
+
 
 class Test_SlackAPI(TestCase):
     def setUp(self):
@@ -15,19 +15,33 @@ class Test_SlackAPI(TestCase):
         self.ch_id = os.environ["SLACK_TEST_CH_ID"]
         
         self.slackAPI = SlackAPI(self.token)
+        
+        self.now = datetime.datetime.now()
+    
     
     def test_check_empty_token(self):
         self.assertFalse(len(self.token)==0)
         
+        
     def test_check_empty_ch_id(self):
         self.assertFalse(len(self.ch_id)==0)    
     
-    def test_send(self):
-        now = datetime.datetime.now()
-        #str_now = now.strftime('%Y-%m-%d %H:%M:%S')
-        try:
-            self.slackAPI.send(self.ch_id, f"{now.strftime('%Y-%m-%d %H:%M:%S')} [unittest] SlackAPI.send()")
-        except Exception as e:
-            print(e)
-            
     
+    def test_send(self):       
+        response = self.slackAPI.send(self.ch_id, f"{self.now.strftime('%Y-%m-%d %H:%M:%S')} [unittest] SlackAPI.send()")
+        self.assertTrue(response["ok"])
+        
+        
+    def test_get_msg(self):
+        response = self.slackAPI.get_msg(self.ch_id, 5)
+        self.assertTrue(response["ok"])
+    
+            
+    def test_get_latest_msg(self):
+        response = self.slackAPI.get_latest_msg(self.ch_id)
+        self.assertTrue(response["ok"])
+        
+        
+    def test_get_channel_list(self):
+        response = self.slackAPI.get_channel_list(5)
+        self.assertTrue(response["ok"])
